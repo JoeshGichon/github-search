4ce626a285a90363d053586b8d6bc800ecef2c18
Joesh Gichon <joeshgichon56@gmail.com>
2021-08-28 18:07:27 +0300

    Putting access token,client id,secret key and base url on the environment folder and hiding them in the gitignore file.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+
 
 export const environment = {
-  production: false
+  production: false,
+  accessToken:"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",
+  baseUrl:"https://api.github.com/users/",
+  clientId:"1c3455f2ce9a56120d1b",
+  clientSecret:"dcbe54eccd5eac548a818f85704bbb1d95274fb4"
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
