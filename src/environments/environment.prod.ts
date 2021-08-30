3901ee1280149972a2172be0f9e640ea3a2d3ea1
Joesh Gichon <joeshgichon56@gmail.com>
2021-08-30 09:22:50 +0300

    moving the client_id,secret_key as well as access_token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  accessToken:"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",
+  baseUrl:"https://api.github.com/users/",
+  clientId:"1c3455f2ce9a56120d1b",
+  clientSecret:"dcbe54eccd5eac548a818f85704bbb1d95274fb4"
 };
